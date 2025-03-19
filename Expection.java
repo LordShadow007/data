63f2e15111fa48b53d758e1da552651649c89e50
Esakki Ram <raesakkiram@gmail.com>
2025-03-19 09:32:37 +0530

    Create Expection.java

---
+++ Expection.java
@@ -0,0 +1,17 @@
+package project;
+
+class IndexOutOfBound extends RuntimeException {
+   public IndexOutOfBound(String message) {
+      super(message);
+   }
+}
+class NoComparableException extends RuntimeException {
+   public NoComparableException(String message) {
+      super(message);
+   }
+}
+class NullValueException extends RuntimeException {
+   public NullValueException(String message) {
+      super(message);
+   }
+}
